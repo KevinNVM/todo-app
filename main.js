0d0625d53ea6bb24cdbb0d47400bbb91c2c0f290
Kevin Darmawan <kevindarmawan022@gmail.com>
2023-04-18 14:25:19 +0700

    first commit

---
+++ main.js
@@ -0,0 +1,72 @@
+const form = document.querySelector("form");
+const listWrapper = document.querySelector("ul#todo-list-wrapper");
+
+/* <li class="list-group-item">
+  <div class="d-flex gap-5 user-select-none">
+    <p>Todo Text</p>
+    <a role="button">
+      <i class="bi bi-trash3 text-danger"></i>
+    </a>
+  </div>
+</li>; */
+
+// Tampilkan todo jika ada di localStorage
+let todoList = localStorage.getItem("todo")
+  ? JSON.parse(localStorage.todo)
+  : [];
+
+refreshTodoUi();
+
+//  Lakukan aksi jika form todo di submit
+form.onsubmit = (e) => {
+  e.preventDefault();
+
+  const formData = new FormData(e.target);
+  const newTodo = generateNewTodo({
+    todoText: formData.get("todo-text"),
+    todoReminder: formData.get("reminder"),
+  });
+
+  todoList = [newTodo, ...todoList];
+  localStorage.setItem("todo", JSON.stringify(todoList));
+  refreshTodoUi();
+};
+
+// Buat object berisi todo dll
+function generateNewTodo({ todoText, todoReminder }) {
+  return {
+    id: Math.random() * 10e6,
+    text: todoText,
+    reminder: todoReminder,
+    createdAt: new Date(),
+  };
+}
+
+// Buat string element todo list
+function generateTodoElement(todo) {
+  return `
+    <li class="list-group-item" data-todo-id="${todo.id}">
+        <div class="d-flex gap-5 user-select-none">
+            <p>${todo.text}</p>
+            <a role="button" onclick="deleteTodo(${todo.id})">
+                <i class="bi bi-trash3 text-danger"></i>
+            </a>
+        </div>
+    </li>
+    `;
+}
+
+// Refresh list todo nya
+function refreshTodoUi() {
+  listWrapper.innerHTML = "";
+  todoList.forEach((todo) => {
+    listWrapper.innerHTML += generateTodoElement(todo);
+  });
+}
+
+// Delete todo dari todo list
+function deleteTodo(id) {
+  todoList = todoList.filter((todo) => todo.id !== id);
+  localStorage.setItem("todo", JSON.stringify(todoList));
+  refreshTodoUi();
+}
